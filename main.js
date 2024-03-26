0fd01297425b3780a65f1842f9f73e262a507c48
elpro99 <147887778+elpro99@users.noreply.github.com>
2024-03-26 14:33:42 -0600

    Update main.js

--- main.js
+++ main.js
@@ -7,16 +7,24 @@ function start(){
 recognition.onresult=function(event){
     console.log(event);
     var content=event.results[0][0].transcript;
+    textbox.innerHTML=content;
     document.getElementById("textbox").innerHTML=content;
     console.log(content);
+    if(content=="Toma mi selfie"){
+        console.log("tomando selfie");
+        speak();
+    }
 }
 camera=document.getElementById("camera");
 function speak(){
     var sint=window.speechSynthesis;
     speakdata=document.getElementById("textbox").value;
-    var utterThis=new SpeechSynthesisUtterance(speakdata);
+    var utterThis=new SpeechSynthesisUtterance(speak_data);
     sint.speak(utterThis);
     Webcam.attach(camera);
+    setTimeout(function(){
+        takeselfie();
+    save();},5000);
 }
 Webcam.set({
     width:360,
@@ -24,3 +32,15 @@ Webcam.set({
     Image_format:'png',
     png_quality:90
 });
+function takeselfie(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie" src="'+data_uri+'"/>';
+    });
+}
+
+function save(){
+    link=document.getElementById("link");
+    image=document.getElementById("selfie").src;
+    link.href=image;
+    link.click();
+}
